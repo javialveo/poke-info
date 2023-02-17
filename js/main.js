519e5f688440c4a37dd2528e1fd3d273611257c3
Eduardo Alveo <eduardo.developer@javialveo.com>
2023-02-17 00:31:31 -0500

    Primera versión del proyecto, se crea una función que inicializa la la web con un pokemón aleatorio. de momento solo muestra su nombre e imagen.

--- js/main.js
+++ js/main.js
@@ -7,4 +7,43 @@ function setAppVersion() {
   label_version.textContent = APP_VERSION;
 }
 
+function addElementToCard(title, picture) {
+  const cardContainer = document.querySelector("#cardContainer");
+  const card = document.createElement("div");
+  const titleCard = document.createElement("h3");
+  const pictureBox = document.createElement("div");
+  const pictureCard = document.createElement("img")
+
+  titleCard.textContent = title;
+  titleCard.setAttribute("class", "card__title");
+
+  pictureCard.setAttribute("src", picture);
+  pictureCard.setAttribute("class", "card__picture");
+
+  pictureBox.appendChild(pictureCard);
+  pictureBox.setAttribute("class", "card__box");
+
+  card.appendChild(titleCard);
+  card.appendChild(pictureBox);
+  card.setAttribute("class", "card");
+
+  cardContainer.appendChild(card);
+
+}
+
+function initPokeInfo() {
+  const card = document.querySelector("#pokeCard");
+
+  const randomNumber = parseInt(Math.random() * 102);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
+  .then(res => res.json())
+  .then(pokedata => {
+    addElementToCard(pokedata.name, pokedata.sprites.other["official-artwork"].front_default);
+    console.log(pokedata.sprites.other["official-artwork"].front_default);
+  });
+}
+
 setAppVersion();
+
+initPokeInfo();
